34b84bd371b8bda1bd44b0e25b9ab85c34ee29c8
Egor Podolskij <podolskijegor51@gmail.com>
2022-09-02 00:27:03 +0300

    Cteate: Adding a list of heroes and information

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,94 @@
-import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import { Component } from "react/cjs/react.production.min";
 
-const CharList = () => {
+import "./charList.scss";
+import MarvelService from "../../services/MarvelService";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+import Spinner from "../spinner/Spinner";
+
+class CharList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    chars: [],
+    loading: true,
+    error: false,
+  };
+
+  marvelService = new MarvelService();
+
+  componentDidMount() {
+    this.marvelService
+      .getAllCharacters()
+      .then(this.onCharsLoaded)
+      .catch(this.onError);
+  }
+
+  onCharsLoaded = (chars) => {
+    this.setState({
+      chars,
+      loading: false,
+    });
+  };
+
+  onError = () => {
+    this.setState({
+      loading: false,
+      error: true,
+    });
+  };
+
+  updateChar = () => {
+    this.marvelService
+      .getAllCharacters()
+      .then(this.onCharsLoaded)
+      .catch(this.onError);
+  };
+
+  render() {
+    const { chars, loading, error } = this.state;
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? (
+      <Char chars={chars} props={this.props} />
+    ) : null;
+
     return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+      <div className="char__list">
+        <ul className="char__grid">
+          {errorMessage}
+          {spinner}
+          {content}
+        </ul>
+        <button className="button button__main button__long">
+          <div className="inner">load more</div>
+        </button>
+      </div>
+    );
+  }
 }
+
+const Char = ({ chars, props }) => {
+  return chars.map((item) => (
+    <li
+      key={item.id}
+      className="char__item"
+      onClick={() => props.onCharSelected(item.id)}
+    >
+      <img
+        style={
+          item.thumbnail ===
+          "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+            ? { objectFit: "contain" }
+            : {}
+        }
+        src={item.thumbnail}
+        alt="Character"
+      />
+      <div className="char__name">{item.name}</div>
+    </li>
+  ));
+};
 
 export default CharList;
